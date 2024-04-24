4844e14571ec08c1edb4f8f9893a37bee04aff2e
CoffeeEnjoyer <kamil.rybak@intel.com>
2024-04-24 19:06:08 +0200

    added md to ts

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "zig", "cpp", "python", "bash", "c", "lua", "vim", "vimdoc", "query", "matlab" },
+  ensure_installed = { "zig", "cpp", "python", "bash", "c", "lua", "vim", "vimdoc", "query", "matlab", "markdown" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
